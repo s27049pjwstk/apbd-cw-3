dd8ac30e7ce0333ae5767ec7b4339152bfad8ca7
s27049 <s27049@pjwstk.edu.pl>
2024-04-05 23:47:02 +0200

    FirstName, LastName and EmailAddress validation moved inside setters

--- zadanie/LegacyApp/User.cs
+++ zadanie/LegacyApp/User.cs
@@ -1,14 +1,42 @@
 ﻿using System;
 
-namespace LegacyApp
-{
-    public class User
-    {
+namespace LegacyApp {
+    public class User {
         public object Client { get; internal set; }
         public DateTime DateOfBirth { get; internal set; }
-        public string EmailAddress { get; internal set; }
-        public string FirstName { get; internal set; }
-        public string LastName { get; internal set; }
+
+        private string _emailAddress;
+
+        public string EmailAddress {
+            get => _emailAddress;
+            internal set {
+                if (!value.Contains("@") && !value.Contains("."))
+                    throw new ArgumentException("Wrong email format!");
+                _emailAddress = value;
+            }
+        }
+
+        private string _firstName;
+
+        public string FirstName {
+            get => _firstName;
+            internal set {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("FirstName Null or Empty!");
+                _firstName = value;
+            }
+        }
+
+        private string _lastName;
+        public string LastName {
+            get => _lastName;
+            internal set {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("LastName Null or Empty!");
+                _lastName = value;
+            }
+        }
+
         public bool HasCreditLimit { get; internal set; }
         public int CreditLimit { get; internal set; }
     }
--- zadanie/LegacyApp/UserService.cs
+++ zadanie/LegacyApp/UserService.cs
@@ -9,37 +9,13 @@ namespace LegacyApp {
         Client GetById(int clientId);
     }
 
-    public class UserService {
-        /*
-        Note
-        UI - html, console
-        BL - logika biznesowa
-        Infrastruktura - I/O
-         */
-
-        private IClientRepository _clientRepository;
-        private ICreditService _creditService;
-
-
-        public UserService() {
-            _clientRepository = new ClientRepository();
-            _creditService = new UserCreditService();
-        }
-
-        public UserService(IClientRepository clientRepository, ICreditService creditService) {
-            _clientRepository = clientRepository;
-            _creditService = creditService;
+    public class UserService(IClientRepository clientRepository, ICreditService creditService) {
+        [Obsolete]
+        public UserService() : this(new ClientRepository(), new UserCreditService()) {
         }
 
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId) {
-            // Logika bizensowa - walidacja
-            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
-                return false;
-
-            // Logika biznesowa - walidacja
-            if (!email.Contains("@") && !email.Contains("."))
-                return false;
-
+            
             // Logika biznesowa
             var now = DateTime.Now;
             int age = now.Year - dateOfBirth.Year;
@@ -49,27 +25,33 @@ namespace LegacyApp {
                 return false;
 
             // Infrastruktura
-            var client = _clientRepository.GetById(clientId);
-
-            var user = new User {
-                Client = client,
-                DateOfBirth = dateOfBirth,
-                EmailAddress = email,
-                FirstName = firstName,
-                LastName = lastName
-            };
+            var client = clientRepository.GetById(clientId);
+            
+            User user;
+            try {
+                 user = new User {
+                    Client = client,
+                    DateOfBirth = dateOfBirth,
+                    EmailAddress = email,
+                    FirstName = firstName,
+                    LastName = lastName
+                };
+            } catch (ArgumentException e) {
+                Console.WriteLine(e);
+                return false;
+            }
 
 
             // Logika biznesowa + Infrastruktura
             if (client.Type == "VeryImportantClient") {
                 user.HasCreditLimit = false;
             } else if (client.Type == "ImportantClient") {
-                int creditLimit = _creditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                int creditLimit = creditService.GetCreditLimit(user.LastName, user.DateOfBirth);
                 creditLimit = creditLimit * 2;
                 user.CreditLimit = creditLimit;
             } else {
                 user.HasCreditLimit = true;
-                int creditLimit = _creditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                int creditLimit = creditService.GetCreditLimit(user.LastName, user.DateOfBirth);
                 user.CreditLimit = creditLimit;
             }
 
